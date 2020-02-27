bb6afa4155ba46904f59c42c54c74f8a9728f6c4
kutalek <davkutalek@gmail.com>
2020-02-26 16:20:03 -0800

    Upgraded to Androidx

--- app/src/main/java/com/usehover/hoverstarter/MainActivity.java
+++ app/src/main/java/com/usehover/hoverstarter/MainActivity.java
@@ -1,12 +1,11 @@
 package com.usehover.hoverstarter;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.hover.sdk.actions.HoverAction;
 import com.hover.sdk.api.Hover;
